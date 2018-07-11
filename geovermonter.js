195525db984f7274aba9184636405bd36cc64a8f
Sam Spencer <sspencerbtv@gmail.com>
2018-07-11 15:43:34 -0400

    got random coords

--- geovermonter.js
+++ geovermonter.js
@@ -1,6 +1,6 @@
+let vermontBoarder = L.geoJSON(border_data)
 
-
-function initialize(border) {
+function initialize() {
 
     map = L.map('mapid').setView([43.942425, -72.698704], 7);
 
@@ -12,7 +12,60 @@ function initialize(border) {
     }).addTo(map);
 
   // create a red polygon from an array of LatLng points
-  let vermontBoarder = L.geoJSON(border_data)
+  
   vermontBoarder.addTo(map)
- 
+
+  let startButton = document.getElementById('start')
+//   startButton.addEventListener('click', () => {
+//     start()
+//   })
 }
+
+function start() {
+    console.log('start')
+    getRandomLatLonInVT()
+}
+
+function guess() {
+    console.log('geuss')
+}
+
+function quit() {
+    console.log('quit')
+}
+
+function getRandomLatLonInVT() {
+
+    let boundingBox = {
+        maxLon: -73.3654,
+        minLon: -71.5489,
+        maxLat: 45.0065,
+        minLat: 42.7395
+    };
+
+    let bb = boundingBox;
+    let randLat = getRandomCoords(bb.minLat, bb.maxLat)
+    let randLon = getRandomCoords(bb.minLon, bb.maxLon)
+
+       
+
+    let point = {lat: randLat, lon: randLon}
+    let inVermont = leafletPip.pointInLayer([randLon, randLat], vermontBoarder);
+
+    if (inVermont.length == 0) {
+        getRandomLatLonInVT()
+       }
+    else {
+        console.log(`Length of results: ${inVermont.length}`)
+        console.log({inVermont})
+        console.log({point})
+    }
+    
+}
+
+
+function getRandomCoords(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+initialize();
